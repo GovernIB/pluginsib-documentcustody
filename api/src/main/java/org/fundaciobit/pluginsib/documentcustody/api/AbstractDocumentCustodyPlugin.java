40bbed6b1cae7866cb3be75344e91e121b4dc484
Antoni Nadal <anadal@fundaciobit.org>
2024-09-09 08:32:29 +0200

    Error el la lectura de Dades Serialitzades a causa del canvi de package de pluginsib-core #15

--- api/src/main/java/org/fundaciobit/pluginsib/documentcustody/api/AbstractDocumentCustodyPlugin.java
+++ api/src/main/java/org/fundaciobit/pluginsib/documentcustody/api/AbstractDocumentCustodyPlugin.java
@@ -1352,6 +1352,14 @@ public abstract class AbstractDocumentCustodyPlugin extends AbstractPluginProper
                     "class=\"org.fundaciobit.pluginsib.documentcustody.api.DocumentCustody\"");
             dataStr = dataStr.replace("class=\"org.fundaciobit.plugins.documentcustody.api.SignatureCustody\"",
                     "class=\"org.fundaciobit.pluginsib.documentcustody.api.SignatureCustody\"");
+            
+            // Canvi de Package de org.fundaciobit.pluginsib.core.utils a org.fundaciobit.pluginsib.core.v3.utils
+            dataStr = dataStr.replace("class=\"org.fundaciobit.pluginsib.core.utils.Metadata\"",
+                    "class=\"org.fundaciobit.pluginsib.core.v3.utils.Metadata\"");
+            dataStr = dataStr.replace("class=\"org.fundaciobit.pluginsib.core.utils.MetadataType\"",
+                    "class=\"org.fundaciobit.pluginsib.core.v3.utils.MetadataType\"");
+            
+            
 
             XMLDecoder decoder = new XMLDecoder(new ByteArrayInputStream(dataStr.getBytes("UTF8")));
             Object o = (Object) decoder.readObject();
